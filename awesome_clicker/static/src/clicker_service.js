3f6cce7adb3bca009c91899427688a2e65c261b7
fdardenne <dafl@odoo.com>
2023-09-29 10:16:28 +0200

    [Solution] 3.6 Buy ClickBots

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -5,15 +5,36 @@ import { reactive } from "@odoo/owl";
 
 const clickerService = {
     start(env) {
-        const state = reactive({ clicks: 0 });
+        const state = reactive({
+            clicks: 0,
+            unlockLevel: 0,
+            clickBots: 0,
+        });
+
+        setInterval(() => {
+            state.clicks += state.clickBots * 10;
+        }, 10000);
 
         function increment(inc) {
             state.clicks += inc;
+            if (state.clicks >= 1000 && state.unlockLevel < 1) {
+                state.unlockLevel++;
+            }
+        }
+
+        function buyClickBot() {
+            const clickBotPrice = 1000;
+            if (state.clicks < clickBotPrice) {
+                return false;
+            }
+            state.clicks -= clickBotPrice;
+            state.clickBots += 1;
         }
 
         return {
             state,
             increment,
+            buyClickBot,
         };
     },
 };
