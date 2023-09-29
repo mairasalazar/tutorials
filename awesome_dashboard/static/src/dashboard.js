3e9fa8f6e00bbfde66fa72ff1868840b6d0f8c44
Géry Debongnie <ged@odoo.com>
2023-09-29 15:31:28 +0200

    update dashboard starting point

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -4,8 +4,7 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.clientaction";
-    static props = ['*'];
+    static template = "awesome_dashboard.AwesomeDashboard";
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
