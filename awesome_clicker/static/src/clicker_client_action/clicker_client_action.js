9e5041013501bbf301d2c05d4e8ad2750d2ca637
fdardenne <dafl@odoo.com>
2023-10-02 17:02:56 +0200

    [Solution] 3.16 Use a Notebook component

--- awesome_clicker/static/src/clicker_client_action/clicker_client_action.js
+++ awesome_clicker/static/src/clicker_client_action/clicker_client_action.js
@@ -4,9 +4,10 @@ import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { ClickerValue } from "../clicker_value/clicker_value";
+import { Notebook } from "@web/core/notebook/notebook";
 class ClickerClientAction extends Component {
     static template = "awesome_clicker.ClickerClientAction";
-    static components = { ClickerValue };
+    static components = { ClickerValue, Notebook };
 
     setup() {
         this.clickService = useState(useService("awesome_clicker.clicker_service"));
