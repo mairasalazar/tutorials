32b30d4596545d80719772bcf4a8f50305aa73d7
fdardenne <dafl@odoo.com>
2023-10-03 13:03:45 +0200

    [Solution] 3.15 Use a dropdown menu for the systray item

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -4,10 +4,12 @@ import { Component, useState, useExternalListener } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { ClickerValue } from "../clicker_value/clicker_value";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
-    static components = { ClickerValue };
+    static components = { ClickerValue, Dropdown, DropdownItem };
 
     setup() {
         this.action = useService("action");
@@ -24,6 +26,22 @@ export class ClickerSystray extends Component {
 
     openClientAction() {
         this.action.doAction("awesome_clicker.dashboard");
+    }
+
+    get numberTrees() {
+        let sum = 0;
+        for (const tree in this.clickService.state.trees) {
+            sum += this.clickService.state.trees[tree].purchased;
+        }
+        return sum;
+    }
+
+    get numberFruits() {
+        let sum = 0;
+        for (const fruit in this.clickService.state.fruits) {
+            sum += this.clickService.state.fruits[fruit];
+        }
+        return sum;
     }
 }
 
