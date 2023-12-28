74bcadc423c4977876201bbacf60c97d0d64aa38
srma-odoo <mpssalazar@gmail.com>
2023-12-28 16:59:57 +0100

    Solving part 1.3

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,9 +2,13 @@
 
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
-    static components = { Counter };
+    static components = { Counter, Card };
+
+    card1 = { content: "card 1", title: "content of card 1" };
+    card2 = { content: "card 2", title: "content of card 2" };
 }
