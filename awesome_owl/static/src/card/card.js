81ae88ccff35cbf17f743559565d6289b39e1e08
srma-odoo <mpssalazar@gmail.com>
2023-12-29 15:31:16 +0100

    Solving part 1.13

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -5,8 +5,5 @@ import { Component } from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card";
 
-    static props = {
-        title: {type: String}, 
-        content: {type: String},
-    }
+    static props = ['title', 'slots']
 }
