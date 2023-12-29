f0ea149b401810d4add1856a5a1fd32631876c09
srma-odoo <mpssalazar@gmail.com>
2023-12-29 08:45:43 +0100

    Solving part 1.5

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,10 +4,12 @@ import { Component, useState } from "@odoo/owl";
 
 export class Counter extends Component {
     static template = "awesome_owl.counter";
+    static props = {onChange: {type: Function, optional:true}};
 
-    state = useState({ value: 0 });
+    state = useState({ value: 1 });
     
     increment() {
         this.state.value++;
+        this.props.onChange?.();
     }
 }
