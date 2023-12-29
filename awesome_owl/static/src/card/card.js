bb1238f130b6b2c1f073003626adab8ce0e24411
srma-odoo <mpssalazar@gmail.com>
2023-12-29 15:41:25 +0100

    Solving part 1.14

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,9 +1,17 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
 
     static props = ['title', 'slots']
+    
+    setup(){
+        this.state = useState({card_open: true});
+    }
+
+    toggleCard() {
+        this.state.card_open = !this.state.card_open;
+    }
 }
